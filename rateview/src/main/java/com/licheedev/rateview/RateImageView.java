4407991aa323a437029396434fd398323245bfb5
licheedev <licheedev@foxmail.com>
2017-08-22 15:35:06 +0800

    first push

---
+++ rateview/src/main/java/com/licheedev/rateview/RateImageView.java
@@ -0,0 +1,77 @@
+package com.licheedev.rateview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+public class RateImageView extends android.support.v7.widget.AppCompatImageView {
+
+    private float rate;
+    private boolean accordingWidth;
+
+    public RateImageView(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public RateImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public RateImageView(@NonNull Context context, @Nullable AttributeSet attrs,
+        @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        if (attrs != null) {
+            TypedArray typedArray =
+                context.obtainStyledAttributes(attrs, R.styleable.RateImageView);
+
+            float widthRate = typedArray.getFloat(R.styleable.RateImageView_width_rate, 0);
+
+            float heightRate = typedArray.getFloat(R.styleable.RateImageView_height_rate, 0);
+
+            accordingWidth = typedArray.getBoolean(R.styleable.RateImageView_according_width, true);
+
+            if (widthRate > 0 && heightRate > 0) {
+
+                if (accordingWidth) {
+                    rate = heightRate / widthRate;
+                } else {
+                    rate = widthRate / heightRate;
+                }
+            } else {
+                rate = 0;
+            }
+            typedArray.recycle();
+        } else {
+            rate = 0;
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (rate > 0) {
+            if (accordingWidth) {
+                int specMode = MeasureSpec.getMode(widthMeasureSpec);
+                int heightSize = (int) (MeasureSpec.getSize(widthMeasureSpec) * rate);
+                int newHeightSpec = MeasureSpec.makeMeasureSpec(heightSize, specMode);
+                super.onMeasure(widthMeasureSpec, newHeightSpec);
+            } else {
+                int specMode = MeasureSpec.getMode(heightMeasureSpec);
+                int widthSize = (int) (MeasureSpec.getSize(heightMeasureSpec) * rate);
+                int newWidthSpec = MeasureSpec.makeMeasureSpec(widthSize, specMode);
+                super.onMeasure(newWidthSpec, heightMeasureSpec);
+            }
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
---
+++ rateview/src/main/java/com/licheedev/rateview/RateRelativeLayout.java
@@ -0,0 +1,89 @@
+package com.licheedev.rateview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.AttrRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.StyleRes;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class RateRelativeLayout extends RelativeLayout {
+
+    private float rate;
+    private boolean accordingWidth;
+
+    public RateRelativeLayout(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public RateRelativeLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public RateRelativeLayout(@NonNull Context context, @Nullable AttributeSet attrs,
+        @AttrRes int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public RateRelativeLayout(@NonNull Context context, @Nullable AttributeSet attrs,
+        @AttrRes int defStyleAttr, @StyleRes int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+        if (attrs != null) {
+            TypedArray typedArray =
+                context.obtainStyledAttributes(attrs, R.styleable.RateLinearLayout);
+
+            float widthRate = typedArray.getFloat(R.styleable.RateLinearLayout_width_rate, 0);
+
+            float heightRate = typedArray.getFloat(R.styleable.RateLinearLayout_height_rate, 0);
+
+            accordingWidth =
+                typedArray.getBoolean(R.styleable.RateLinearLayout_according_width, true);
+
+            if (widthRate > 0 && heightRate > 0) {
+
+                if (accordingWidth) {
+                    rate = heightRate / widthRate;
+                } else {
+                    rate = widthRate / heightRate;
+                }
+            } else {
+                rate = 0;
+            }
+            typedArray.recycle();
+        } else {
+            rate = 0;
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (rate > 0) {
+            if (accordingWidth) {
+                int specMode = MeasureSpec.getMode(widthMeasureSpec);
+                int heightSize = (int) (MeasureSpec.getSize(widthMeasureSpec) * rate);
+                int newHeightSpec = MeasureSpec.makeMeasureSpec(heightSize, specMode);
+                super.onMeasure(widthMeasureSpec, newHeightSpec);
+            } else {
+                int specMode = MeasureSpec.getMode(heightMeasureSpec);
+                int widthSize = (int) (MeasureSpec.getSize(heightMeasureSpec) * rate);
+                int newWidthSpec = MeasureSpec.makeMeasureSpec(widthSize, specMode);
+                super.onMeasure(newWidthSpec, heightMeasureSpec);
+            }
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
